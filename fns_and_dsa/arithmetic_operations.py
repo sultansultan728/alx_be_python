6a8fd014c09cefaa9a6ca1ec80e034213f067fa0
sultansultan728 <sultansultan7659999@gmail.com>
2025-09-25 07:05:22 -0700

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Perform basic arithmetic operations based on the given operation string.
     
